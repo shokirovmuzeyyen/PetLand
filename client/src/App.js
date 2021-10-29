49402f02a199a8485223c9f2427dc32b2751999a
fn-malkap <muzeyyen.alkap@faraday.net>
2021-10-29 13:07:30 +0300

    caches

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>CRUD Apllication</h1>
+      <h1>CRUD Application</h1>
 
       <div className="form">
         <label>Movie Name</label>
         <input type="text" name="movieName" />
         <label>Review</label>
         <input type="text" name="review" />
+
+        <button>Submit</button>
+      </div>
+
+      <h1>SIGN IN</h1>
+      <div className="form">
+        <label>E-mail address</label>
+        <input type="text" name="mail" />
+        <label>Password</label>
+        <input type="text" name="password" />
 
         <button>Submit</button>
       </div>
