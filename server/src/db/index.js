b540f91fc8c29f831984908f158acbe6fca9c8f8
zeynepakant <zeynepakant@sabanciuniv.edu>
2021-11-01 16:20:18 +0300

    heroku-db

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,10 +1,11 @@
 const { Pool } = require('pg')
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'petland',
-  password: '1234',
+  user: 'jukxmvjmwjawqi',
+  host: 'ec2-52-208-221-89.eu-west-1.compute.amazonaws.com',
+  database: 'dcbcogbu1bjnl4',
+  password: '652dc50927382af8ef8cdb95d2bc9bab85bc165b12330d5254192b8ad0858faa',
   port: 5432,
+  ssl: { rejectUnauthorized: false }
 })
 
 module.exports = {
