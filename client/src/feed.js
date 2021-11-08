adeafaff454e33a7c6a8beb790cb262cf7dea6d1
fn-malkap <muzeyyen.alkap@faraday.net>
2021-11-08 16:28:36 +0300

    Feed Page

--- client/src/feed.js
+++ client/src/feed.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const feed = () => {
   return (
-    <div>
-      <h1>FEED</h1>
+    <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
+      <h1>FEED PAGE</h1>
+      <h2>-- Posts will be here</h2>
     </div>
   )
 }
