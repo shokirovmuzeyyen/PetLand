1b4a8df3e35c39ab936f1c441f86eccf3d9ecf95
alkapmuzeyyen <69919832+alkapmuzeyyen@users.noreply.github.com>
2021-11-15 23:04:05 +0300

    Update index.js

--- server/src/constants/index.js
+++ server/src/constants/index.js
@@ -3,7 +3,7 @@ config()
 
 module.exports = {
   PORT: process.env.PORT,
-  SERVER_URL: process.env.SERVER_URL,
+  SERVER_URL: process.env.SERVER_URL || https://petland-app.herokuapp.com/,
   CLIENT_URL: process.env.CLIENT_URL,
   SECRET: process.env.SECRET,
 }
