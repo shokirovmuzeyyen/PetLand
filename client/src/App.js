05fe59ff13b82554c1e0b76f6ef07c2f90ae404f
= <=>
2021-11-17 21:04:35 +0300

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Login from './login';
 import Register from './register';
 import Welcome from './welcome';
 import Feed from './feed';
+import DM from './dm';
+import Profile from './profile';
 import CreatePost from './CreatePost';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 //run npm install react-router-dom --save
@@ -20,6 +22,8 @@ function App() {
       <Route path="/register" component={Register} />
       <Route path="/feed" component={Feed} />
       <Route path="/createPost" component={CreatePost} />
+      <Route path="/dm" component={DM} />
+      <Route path="/profile" component={Profile} />
       </Switch>
     </Router>
   );
