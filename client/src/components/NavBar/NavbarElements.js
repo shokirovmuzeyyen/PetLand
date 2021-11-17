bdea93a7b26927f1cd65e558255322633ac3e658
= <=>
2021-11-17 21:06:43 +0300

    navbar

---
+++ client/src/components/NavBar/NavbarElements.js
@@ -0,0 +1,17 @@
+export const NavBarElements = [
+  {
+    title: 'Home',
+    url: '#',
+    cName: 'nav-links'
+  },
+  {
+    title: 'DM',
+    url: '/dm',
+    cName: 'nav-links'
+  },
+  {
+    title: 'Profile',
+    url: '/profile',
+    cName: 'nav-links'
+  }
+]
