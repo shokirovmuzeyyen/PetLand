d8ffec2324c66a7539eb1b2df9f47891e3d71b7c
= <=>
2021-12-04 19:25:16 +0300

    search

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -63,7 +63,7 @@ class Navbar extends Component{
   render(){
     return(
       <nav className="NavbarItems">
-        <h1 className="navbar-logo">PetLand<i className="fab fa-react"></i></h1>
+        <h1 className="navbar-logo">PetLand<i className="fab fa-thin fa-react"></i></h1>
         <div className="menu-icon" onClick={this.handleClick}>
           <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
         </div>
--- client/src/search.js
+++ client/src/search.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { useHistory, Redirect } from "react-router-dom";
 import Axios from 'axios';
 
@@ -14,38 +14,33 @@ import {
 import { config } from './config';
 import PropTypes from 'prop-types';
 import useToken from './useToken';
-
-async function get_username(user_id){
-  console.log("in");
-  Axios.post('http://localhost:8000/api/search',
-  {
-    id: user_id,
-  }).then((response) => {
-    const name = response.data.name;
-    console.log("name:", name);
-    if (!response){
-      console.log("no error");
-    }
-    else{
-      console.log(response);
-    }
-    return name;
-    }).catch(error => {
-      console.log(error);
-      let err = error.response.data.errors[0].msg;
-      console.log(err);
-  });
-}
+import axios from 'axios';
 
 
-export default async function Search(){
-    const user_id = sessionStorage.getItem('token');  
-    console.log(user_id);
-    let name = await get_username(user_id);
-    console.log(name);
-    return (
-      <div>
-        <h1>{name}</h1>
+export default function Search(){
+  const {searchTerm, setSearchTerm} = useState('')
+  const {posts, setPosts} = useState([])
+  useEffect(() => {
+    axios.post('http://localhost:8000/api/search_all')
+    .then((response) => {
+    console.log(response.data);
+    setPosts(response.data);
+    })
+  }, [])
+
+  
+  return (
+    <div className="search">
+      <input 
+      type="text" 
+      placeholder="Search..." 
+      onChange={event => {setSearchTerm(event.target.value)}}/>
+      {posts.map((val)=> {
+        return <div key={val.id}>
+          <p>{val.name}</p>
+        </div>
+        
+      })}
       </div>
-      );
+  );
 }
