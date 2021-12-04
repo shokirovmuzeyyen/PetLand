68a9822358d1c2c63d0b46439aece060a473d59d
= <=>
2021-12-04 13:01:16 +0300

    session

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,13 +1,65 @@
-import React, {Component} from 'react'
+import React, {useEffect, Component} from 'react'
 import {NavBarElements} from './NavbarElements'
 import './NavBar.css'
 import {Button} from 'reactstrap';
+import Axios from 'axios';
+import useToken from '../../useToken';
+import { useHistory } from "react-router-dom";
+import {  Link, Redirect } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
-class Navbar extends Component {
+import { useState } from 'react';
+
+class Navbar extends Component{ 
+  constructor(props) {
+    super(props);
+    console.log(this.props);
+     const { history } = this.props;
+    this.routingFunction = this.routingFunction.bind(this);
+    //this.logout() = this.logout.bind(this);
+  }
+
+  routingFunction = () => {
+    this.props.history.push({
+        pathname: '/feed',
+        //state: param
+    });
+  }
+ 
+
   state = {clicked: false}
   handleClick =() => {
     this.state({clicked: !this.state.clicked})
-  }
+  };
+
+  async logout(e) {
+    console.log("out");
+    Axios.post('http://localhost:8000/api/logout',
+    {
+      logout: true,   
+    }).then((response) => {
+      if (!response){
+        console.log("no error");
+      }
+      else{
+        console.log(response);
+        sessionStorage.removeItem('token');
+        //history.push('/feed')   
+        this.props.history.push("/feed");
+      }
+
+    }).catch(error => {
+        console.log(error);
+        let err = error.response;
+        if (err){
+          console.log(err);
+        }});
+     
+    };
+
+
+
   render(){
     return(
       <nav className="NavbarItems">
@@ -26,10 +78,13 @@ class Navbar extends Component {
               )})
           }
         </ul>
-        <Button href="/login" >Log Out</Button>
+        <Button onClick={this.logout}>Log Out</Button>
       </nav>
     )
   }
 }
 
-export default Navbar
+
+
+export default  withRouter(Navbar)
+
