43597e12ae70988b4ae99b5d049235de7f095eed
zeynepakant <zeynepakant@sabanciuniv.edu>
2021-12-05 16:52:16 +0300

    db-config

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg')
 const pool = new Pool({
-  user: 'jukxmvjmwjawqi',
-  host: 'ec2-52-208-221-89.eu-west-1.compute.amazonaws.com',
-  database: 'dcbcogbu1bjnl4',
-  password: '652dc50927382af8ef8cdb95d2bc9bab85bc165b12330d5254192b8ad0858faa',
-  port: 5432,
+  user: process.env.DATABASE_USER,
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD,
+  port: process.env.DATABASE_PORT,
   ssl: { rejectUnauthorized: false }
 })
 
